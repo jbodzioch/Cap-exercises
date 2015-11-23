99d3ee095336c960293fbbd328a9dd4ff9bd1c95
jbodzioch <jan.a.bodzioch@gmail.com>
2015-11-23 14:56:39 +0100

    Refactored MVC

--- javaExercises/GameOfLife/src/main/java/com/capgemini/gameOfLife/controller/Main.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.gameOfLife.controller;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Controller controller = new Controller();
-		controller.startThis();
-	}
-}
