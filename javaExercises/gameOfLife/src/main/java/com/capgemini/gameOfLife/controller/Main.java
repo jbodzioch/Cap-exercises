fe606cf8bd0fea1a29ed2543894b943d6c8df8bb
jbodzioch <jan.a.bodzioch@gmail.com>
2015-11-23 14:58:24 +0100

    No idea what is happening

---
+++ javaExercises/gameOfLife/src/main/java/com/capgemini/gameOfLife/controller/Main.java
@@ -0,0 +1,9 @@
+package com.capgemini.gameOfLife.controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		controller.startThis();
+	}
+}
